2b8d7ba1e80a0306e0489874e8203fe3c66af465
Assoumani <assoumani.mutabaruka@gmail.com>
2020-01-10 17:06:59 +0100

    fix codacy errors

--- public/index.php
+++ public/index.php
@@ -6,7 +6,7 @@ use App\Service\WeatherService;
 require './../vendor/autoload.php';
 
 $controller = new WeatherController();
-if (count($_POST) > 0) {
+if (count(filter_input_array(INPUT_POST)["city"]) > 0) {
     $controller->showByCity();
 } else {
     $controller->show();
